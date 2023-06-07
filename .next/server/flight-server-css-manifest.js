bc4b836d5fed5a4b939eec77e956c8f1e55e7726
kellcrivelaro <kell_crivelaro@hotmail.com>
2023-06-07 17:48:29 -0300

    fix:Adicionando pasta next no .gitignore

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,16 +1,4 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "C:\\Users\\Kell\\Documents\\Projetos\\frontend-challenge\\src\\app\\layout.tsx": [
-      "C:\\Users\\Kell\\Documents\\Projetos\\frontend-challenge\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Saira\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"300\",\"400\",\"500\",\"600\"],\"variable\":\"--font-sans\"}],\"variableName\":\"saira\"}",
-      "C:\\Users\\Kell\\Documents\\Projetos\\frontend-challenge\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Saira_Stencil_One\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"400\",\"variable\":\"--font-logo\"}],\"variableName\":\"sairaStencil\"}",
-      "C:\\Users\\Kell\\Documents\\Projetos\\frontend-challenge\\src\\app\\globals.css"
-    ]
-  },
-  "cssModules": {
-    "C:\\Users\\Kell\\Documents\\Projetos\\frontend-challenge\\src\\app\\page": [
-      "C:\\Users\\Kell\\Documents\\Projetos\\frontend-challenge\\src\\app\\globals.css",
-      "C:\\Users\\Kell\\Documents\\Projetos\\frontend-challenge\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Saira\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"300\",\"400\",\"500\",\"600\"],\"variable\":\"--font-sans\"}],\"variableName\":\"saira\"}",
-      "C:\\Users\\Kell\\Documents\\Projetos\\frontend-challenge\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Saira_Stencil_One\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"400\",\"variable\":\"--font-logo\"}],\"variableName\":\"sairaStencil\"}"
-    ]
-  }
+  "cssImports": {},
+  "cssModules": {}
 }
