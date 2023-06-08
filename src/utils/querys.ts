2fb5053db62e9eda27962a8eb4c76d043f13976a
kellcrivelaro <kell_crivelaro@hotmail.com>
2023-06-08 16:38:47 -0300

    refactor:Config. de conexão com GraphQL hospedado na Hasura e querys necessárias

--- src/utils/querys.ts
+++ src/utils/querys.ts
@@ -2,39 +2,43 @@ import { gql } from '@apollo/client'
 import { FilterCategories, FilterPriorities } from './types'
 
 function getCategoryByType(category: FilterCategories) {
-  if (category === FilterCategories.MUG) return `filter: {category: "mugs"}`
+  if (category === FilterCategories.MUG)
+    return `where: {category: {_eq: "mugs"}}`
   if (category === FilterCategories.SHIRT)
-    return `filter: {category: "t-shirts"}`
+    return `where: {category: {_eq: "t-shirts"}}`
   return ''
 }
 
 function getFieldByPriority(priority: FilterPriorities) {
   if (priority === FilterPriorities.POPULARITY)
-    return { field: 'sales', order: 'DSC' }
+    return ', order_by: {sales: desc}'
   if (priority === FilterPriorities.BIGGEST_PRICE)
-    return { field: 'price_in_cents', order: 'DSC' }
+    return ', order_by: {price_in_cents: desc}'
   if (priority === FilterPriorities.MINOR_PRICE)
-    return { field: 'price_in_cents', order: 'ASC' }
-  return { field: 'created_at', order: 'DSC' }
+    return ', order_by: {price_in_cents: asc}'
+  return ', order_by: {created_at: desc}'
 }
 
 export function getProducts(
   category: FilterCategories,
   priority: FilterPriorities,
 ) {
-  const sortSettings = getFieldByPriority(priority)
   const categoryFilter = getCategoryByType(category)
+  const sortSettings = getFieldByPriority(priority)
 
   return gql`
-  query {
-    allProducts(sortField: "${sortSettings.field}", sortOrder: "${sortSettings.order}", ${categoryFilter}) {
-      id
-      name
-      price_in_cents
-      image_url
-      category
+    query {
+      allProducts (${categoryFilter} ${sortSettings}){
+        id
+        name
+        description
+        category
+        image_url
+        price_in_cents
+        sales
+        created_at
+      }
     }
-  }
   `
 }
 
@@ -43,19 +47,85 @@ export function getProductsByPage(
   priority: FilterPriorities,
   page: number,
 ) {
-  const sortSettings = getFieldByPriority(priority)
   const categoryFilter = getCategoryByType(category)
+  const sortSettings = getFieldByPriority(priority)
+  const numberPerPage = 12
 
   return gql`
-  query {
-    allProducts(sortField: "${sortSettings.field}", sortOrder: "${sortSettings.order}", ${categoryFilter}, 
-    page: ${page}, perPage: 12) {
-      id
-      name
-      price_in_cents
-      image_url
-      category
+    query {
+      allProducts (${categoryFilter} ${sortSettings}, limit: ${numberPerPage}, offset: ${
+    numberPerPage * page
+  }){
+        id
+        name
+        description
+        category
+        image_url
+        price_in_cents
+        sales
+        created_at
+      }
     }
-  }
   `
 }
+
+// LocalHost
+
+// function getCategoryByType(category: FilterCategories) {
+//   if (category === FilterCategories.MUG) return `filter: {category: "mugs"}`
+//   if (category === FilterCategories.SHIRT)
+//     return `filter: {category: "t-shirts"}`
+//   return ''
+// }
+
+// function getFieldByPriority(priority: FilterPriorities) {
+//   if (priority === FilterPriorities.POPULARITY)
+//     return { field: 'sales', order: 'DSC' }
+//   if (priority === FilterPriorities.BIGGEST_PRICE)
+//     return { field: 'price_in_cents', order: 'DSC' }
+//   if (priority === FilterPriorities.MINOR_PRICE)
+//     return { field: 'price_in_cents', order: 'ASC' }
+//   return { field: 'created_at', order: 'DSC' }
+// }
+
+// export function getProducts(
+//   category: FilterCategories,
+//   priority: FilterPriorities,
+// ) {
+//   const sortSettings = getFieldByPriority(priority)
+//   const categoryFilter = getCategoryByType(category)
+
+//   return gql`
+//   query {
+//     allProducts(sortField: "${sortSettings.field}", sortOrder: "${sortSettings.order}", ${categoryFilter}) {
+//       id
+//       name
+//       price_in_cents
+//       image_url
+//       category
+//     }
+//   }
+//   `
+// }
+
+// export function getProductsByPage(
+//   category: FilterCategories,
+//   priority: FilterPriorities,
+//   page: number,
+// ) {
+//   const sortSettings = getFieldByPriority(priority)
+//   const categoryFilter = getCategoryByType(category)
+
+//   return gql`
+//   query {
+//     allProducts(sortField: "${sortSettings.field}", sortOrder: "${sortSettings.order}", ${categoryFilter},
+//     page: ${page}, perPage: 12) {
+//       id
+//       name
+//       price_in_cents
+//       image_url
+//       category
+//     }
+//   }
+//   `
+// }
